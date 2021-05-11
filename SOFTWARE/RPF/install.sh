7bb94a67ea4fc0652383ae3a5a1dcc870ecc4832
mainframed <mainframed767@gmail.com>
2021-05-10 17:03:38 -0700

    Changing software location from root to SOFTWARE

---
+++ SOFTWARE/RPF/install.sh
@@ -0,0 +1,6 @@
+# Installs RPF
+# This installs RPF V1R8M8 and up
+# For use with Jay Moseley sysgen MVS only
+echo "Checking for new RPF version"
+bash $(dirname $0)/update_rpf.sh
+
---
+++ SOFTWARE/RPF/install_rpf.sh
@@ -0,0 +1,29 @@
+# Installs RPF
+# This installs RPF V1R8M8 and up
+# For use with Jay Moseley sysgen MVS only
+
+source ../00_sysgen_functions.sh
+trap 'check_return' 0
+
+echo_step "Checking for new RPF version"
+./update_rpf.sh
+
+set -e
+
+ipl_install
+
+cd ../sysgen
+echo_step "Starting Hercules: hercules -f conf/local.cnf -r ../SOFTWARE/RPF/install_rpf.rc"
+hercules -f conf/local.cnf -r ../SOFTWARE/RPF/install.rc > hercules.log
+
+echo_step "Backing up hercules.log to hercules_log.rpf_install.$date_time.log"
+mv hercules.log hercules_log.rpf_install.$date_time.log
+echo_step "Backing up prt00e.txt to prt00e.rpf_install.$date_time.txt"
+cp prt00e.txt prt00e.rpf_install.$date_time.txt
+
+clean_up
+
+trap : 0
+
+
+
