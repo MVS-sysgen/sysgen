7c11a29a785d18924121d5d4fb5a6711e393a905
mainframed <mainframed767@gmail.com>
2021-06-07 17:58:42 -0700

    removing uneeded SOC1 fix

--- SOFTWARE/REVIEW/update_review.sh
+++ SOFTWARE/REVIEW/update_review.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 # Downloads newest REVIEW
 
-function echoc {
-    echo "$(tput bold)$(tput setaf 3)[+] $1$(tput sgr0)"
-}
 
 cd $(dirname $0)
 
-echoc "Getting newest RECV370"
+echo "Getting newest RECV370"
 #wget "http://www.prycroft6.com.au/vs2sw/download/rev370.zip"
 wget "http://www.prycroft6.com.au/REVIEW/download/revhelp.zip"
 wget "http://www.prycroft6.com.au/REVIEW/download/rev370ld.zip"
@@ -19,5 +16,3 @@ for i in *.zip; do
 done
 mkdir -p zip
 mv *.zip zip
-
-
