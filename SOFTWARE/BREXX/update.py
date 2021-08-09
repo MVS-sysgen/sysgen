ed1d3f6495778d49024e13ed1747dd311d7be10e
mainframed <mainframed767@gmail.com>
2021-08-09 10:59:06 -0700

    added decode to support older python3 version

--- SOFTWARE/BREXX/update.py
+++ SOFTWARE/BREXX/update.py
@@ -7,10 +7,15 @@ import os
 user = "mvslovers"
 repo = "brexx370"
 
-_json = json.loads(urllib.request.urlopen(urllib.request.Request(
-    'https://api.github.com/repos/{u}/{r}/releases/latest'.format(u=user,r=repo),
-     headers={'Accept': 'application/vnd.github.v3+json'},
-)).read())
+_json = json.loads(
+            urllib.request.urlopen(
+                urllib.request.Request(
+                    'https://api.github.com/repos/{u}/{r}/releases/latest'.format(u=user,r=repo),
+                    headers={'Accept': 'application/vnd.github.v3+json'},
+                )
+            ).read().decode('utf-8')
+        )
+
 asset = _json['assets'][0]
 
 print(_json['tag_name'])
