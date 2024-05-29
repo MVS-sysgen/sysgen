bc7f748f4f566bd07c769c89e1b21831b3e744f2
Bill Lewis <wrljet@gmail.com>
2024-05-29 17:30:57 -0400

    Overwrite existing MVSCI/SCRIPTS directory
    This makes the process more restartable.

--- sysgen.py
+++ sysgen.py
@@ -2225,7 +2225,9 @@ class sysgen:
         Path(running_folder+"MVSCE/punchcards").mkdir(parents=True, exist_ok=True)
 
         self.print("Copying scripts to {}".format(Path(running_folder+"MVSCE/SCRIPTS").resolve()))
-        shutil.copytree(Path('SCRIPTS').resolve(), Path(running_folder+"MVSCE/SCRIPTS").resolve())
+
+        # WRL overwrite existing target directory
+        shutil.copytree(Path('SCRIPTS').resolve(), Path(running_folder+"MVSCE/SCRIPTS").resolve(), dirs_exist_ok=True)
 
         up = "| {:9} | {:8}|\n"
 
